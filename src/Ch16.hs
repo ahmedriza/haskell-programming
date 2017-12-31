20649053cf9ca995530cc0423f58c464fb68e3c0
Ahmed Riza <dr.ahmed_riza@yahoo.co.uk>
2017-12-31 18:44:10 +0000

    Ch17

--- src/Ch16.hs
+++ src/Ch16.hs
@@ -1,10 +1,13 @@
 -- {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Ch16 where
 
 import Test.QuickCheck
 import Test.QuickCheck.Function
+import GHC.Arr
 
 ---------------------------
 -- Chapter 16
@@ -468,4 +471,295 @@ test3 = do
 -- Ignoring Possibilities
 -- =============================================================================
 
+incIfJust :: Num a => Maybe a -> Maybe a
+incIfJust (Just n) = Just $ n + 1
+incIfJust Nothing  = Nothing
 
+showIfJust :: Show a => Maybe a -> Maybe String
+showIfJust (Just s) = Just $ show s
+showIfJust Nothing = Nothing
+
+incMaybe :: Num a => Maybe a -> Maybe a
+incMaybe m = fmap (+1) m
+
+showMaybe :: Show a => Maybe a -> Maybe String
+showMaybe m = fmap show m
+
+incMaybe' :: Num a => Maybe a -> Maybe a
+incMaybe' = fmap (+1)
+
+showMaybe' :: Show a => Maybe a -> Maybe String
+showMaybe' = fmap show
+
+liftedInc :: (Functor f, Num a) => f a -> f a
+liftedInc = fmap (+1)
+
+liftedShow :: (Functor f, Show a) => f a -> f String
+liftedShow = fmap show
+
+-- Exercise
+
+-- Write a Functor instance for a datatype identical to Either. We'll use our own
+-- datatype
+
+data Sum a b = FirstClass a | SecondClass b deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap _ (FirstClass a) = FirstClass a
+  fmap f (SecondClass b) = SecondClass (f b)
+
+-- =============================================================================
+-- A Somewhat Surpsing Functor
+-- =============================================================================
+
+c1 = const 20 30
+
+data Constant a b = Constant { getConstant :: a } deriving (Eq, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant a) = Constant a
+
+c2 = Constant 20
+
+-- =============================================================================
+-- More structure, more functors
+-- =============================================================================
+
+data Wrap f a = Wrap (f a) deriving (Eq, Show)
+
+instance (Functor f) => Functor (Wrap f) where
+  fmap g (Wrap (fa)) = Wrap (fmap g fa)
+
+w1 = Wrap (Just 10)
+
+data Or' a b = First' a | Second' b deriving (Eq, Show)
+w2= Wrap (First' 20)
+
+-- =============================================================================
+-- IO Functor
+-- =============================================================================
+
+ioInt :: IO Int
+ioInt = readIO "10" :: IO Int
+
+ioInt' :: IO Int
+ioInt' = fmap (+1) ioInt -- equals 11
+
+-- =============================================================================
+-- What if we want to do something different?
+-- =============================================================================
+
+-- Natural transformation: leave the values untouched, but transform the structure.
+
+type Nat f g = forall a . f a -> g a
+
+-- Example:
+
+maybeToList :: Nat Maybe []
+-- maybeToList :: Maybe t -> [t]
+maybeToList Nothing = []
+maybeToList (Just a) = [a]
+
+-- =============================================================================
+-- Functors are unique to a datatype
+-- =============================================================================
+
+-- =============================================================================
+-- Chapter Exercises
+-- =============================================================================
+
+-- Determine if a valid Functor can be written for the datatype provided.
+
+-- (1)
+data MyBool = MyFalse | MyTrue
+-- Answer: No
+
+-- (2)
+data BoolAndSomethingElse a = False' a | True' a
+
+instance Functor (BoolAndSomethingElse) where
+  fmap f (False' a) = False' (f a)
+  fmap f (True' a) = True' (f a)
+
+-- (3)
+data BoolAndMaybeSomethingElse a = Falish | Truish a
+instance Functor (BoolAndMaybeSomethingElse) where
+  fmap f (Falish) = Falish
+  fmap f (Truish a) = Truish (f a)
+
+-- (4) Use kinds to guide you on this one
+newtype Mu f = InF { outF :: f (Mu f) }
+-- instance Functor (Mu (f a)) where
+-- Not possible, since kind of Mu is: (* -> *) -> *
+
+-- (5) Use kinds again
+data D = D (Array Word Word) Int Int
+-- Not possible, since kind of D is: *
+
+-- Re-arrange arguments to the type constructor of the datatype so that the Functor works.
+
+-- (1)
+-- data MySum a b = MyFirst a | MySecond b
+
+data MySum b a = MyFirst a | MySecond b
+instance Functor (MySum e) where
+  fmap f (MyFirst a) = MyFirst (f a)
+  fmap f (MySecond b) = MySecond b
+
+-- (2)
+data Company a c b = DeepBlue a c | Something b
+instance Functor (Company e e') where
+  fmap f (Something b) = Something (f b)
+  fmap _ (DeepBlue a c) = DeepBlue a c
+
+-- (3)
+-- fmap (+1) (L 1 2 3) == L 2 2 4
+-- fmap (+1) (R 1 2 3) == R 1 3 3
+data More b a = L a b a | R b a b deriving (Eq, Show)
+instance Functor (More x) where
+  fmap f (L a b a') = L (f a) b (f a')
+  fmap f (R b a b') = R b (f a) b'
+
+-- Write Functor instances for the following datatypes
+
+-- (1)
+data Quant a b = Finance | Desk a | Bloor b
+instance Functor (Quant a) where
+  fmap _ Finance = Finance
+  fmap f (Desk a) = Desk a
+  fmap f (Bloor b) = Bloor (f b)
+
+-- (2)
+-- data K a b = K a
+
+newtype K a b = K a  deriving (Eq, Show)
+
+instance Functor (K a) where
+  fmap _ (K a) = K a
+
+-- (3)
+newtype Flip f a b = Flip (f b a) deriving (Eq, Show)
+
+instance Functor (Flip K a) where
+  fmap f (Flip (K a)) = Flip $ K (f a)
+
+-- (4)
+data EvilGoateeConst a b = GoatyConst b deriving (Eq, Show)
+instance Functor (EvilGoateeConst a) where
+  fmap f (GoatyConst b) = GoatyConst (f b)
+
+-- (5) Do you need something extra to make the instance work
+data LiftItOut f a = LiftItOut (f a) deriving (Eq, Show)
+
+instance Functor f => Functor (LiftItOut f) where
+  fmap g (LiftItOut fa) = LiftItOut (fmap g fa)
+
+lifted :: LiftItOut Maybe Integer
+lifted = LiftItOut (Just 10)
+
+-- (6)
+data DaWrappa f g a = DaWrappa (f a) (g a) deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (DaWrappa f g) where
+  fmap h (DaWrappa f g) = DaWrappa (fmap h f) (fmap h g)
+
+dw1 :: DaWrappa Maybe Maybe Integer
+dw1 = DaWrappa (Just 10) (Just 20)
+
+dw1' = fmap (+1) dw1 == DaWrappa (Just 11) (Just 21)
+
+-- (7)
+data IgnoreOne f g a b = IgnoringSomething (f a) (g b) deriving (Eq, Show)
+
+instance (Functor g) => Functor (IgnoreOne f g a) where
+  -- apply h only to the second argument b
+  fmap h (IgnoringSomething fa gb) = IgnoringSomething fa (fmap h gb) 
+
+ignore1 = IgnoringSomething (Just 20) (Just 30)
+
+ignore1' = fmap (+1) ignore1
+
+-- (8)
+data Notorious g o a t = Notorious (g o) (g a) (g t) deriving (Eq, Show)
+
+instance Functor g => Functor (Notorious g o a) where
+  -- apply h only to the 3rd argument t
+  fmap h (Notorious go ga gt) = Notorious go ga (fmap h gt)
+
+notorious = Notorious (Just 10) (Just 20) (Just 30)
+notorious' = fmap (+1) notorious
+
+-- (9) You'll need to use recursion
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+instance Functor (List) where
+  fmap f Nil = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+list1 = Cons 10 (Nil)
+list2 = Cons 20 list1
+
+list2' = fmap (+1) list2 == Cons 21  (Cons 11 Nil)
+
+-- (10) A tree of goats forms a Goat-Lord, fearsome poly-creature
+
+data GoatLord a = NoGoat
+                  | OneGoat a
+                  | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+                  deriving (Eq, Show)
+
+instance Functor (GoatLord) where
+  fmap f NoGoat = NoGoat
+  fmap f (OneGoat x) = OneGoat (f x)
+  fmap f (MoreGoats gl1 gl2 gl3) = MoreGoats (fmap f gl1) (fmap f gl2) (fmap f gl3) 
+
+ng = NoGoat
+g1 = OneGoat 1
+mg1 = MoreGoats ng (OneGoat 2) (OneGoat 3)
+mg2 = MoreGoats g1 g1 mg1 -- == MoreGoats (OneGoat 1) (OneGoat 1) (MoreGoats NoGoat (OneGoat 2) (OneGoat 3))
+ 
+mg2' = fmap (+1) mg2 == MoreGoats (OneGoat 2) (OneGoat 2) (MoreGoats NoGoat (OneGoat 3) (OneGoat 4))
+
+-- (11) You'll use an extra functor for this one, although your solution might do it
+-- monomorphically without using fmap.  Keep in mind that you will probably not be able to validate
+-- this one in the usual manner.  Do your best to make it work.
+
+instance Show (String -> a) where
+  show a = "(String -> a)"
+
+newtype Reader a = Read' (String -> a)
+instance Functor (Reader) where
+  fmap f (Read' g) = Read' (fmap f g)
+
+apply :: a -> (Reader a) -> a
+apply x ra = undefined
+
+r1 = Read' (\s-> read s :: Integer)  
+
+data TalkToMe a = Halt
+                  | Print String a
+                  | Reader a -- Read' (String -> a)
+                  deriving Show
+
+instance Functor (TalkToMe) where
+  fmap _ Halt = Halt
+  fmap g (Print s x) = Print s (g x)
+  -- fmap g (Read' f) = Read' (fmap g f)
+
+-- instance Functor (Read g a) where
+--  fmap f (Read g) = undefined
+  
+tt1 :: TalkToMe a
+tt1 = Halt
+
+tt2 :: TalkToMe Integer
+tt2 = Print "hello" 10
+tt2' = fmap (+1) tt2 -- == Print "hello" 11
+
+-- tt3 :: TalkToMe Integer
+-- tt3 = Read' (\s -> read s :: Integer)
+
+-- fmap (+10) (\s -> read s :: Integer) ("11") == 21
+
+-- tt3' :: TalkToMe Integer
+--tt3' = fmap (+1) tt3
---
+++ src/Ch17.hs
@@ -0,0 +1,244 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Ch17 where
+--
+-- Chapter 17: Applicative
+--
+
+import Control.Applicative
+import Data.Monoid
+
+--- Note how the 1st Monoid instance of these tuples get combined
+
+p1 = ("Woo", (+1)) <*> (" Hoo!", 1) -- == ("Woo Hoo!", 2)
+
+p2 = (Sum 2, (+1)) <*> (Sum 1, 10)  -- == (Sum 3, 11)
+
+p3 = (Product 3, (+9)) <*> (Product 2, 8) -- == (Product 6, 17)
+
+p4 = (All True, (+1)) <*> (All False, 1)  -- == (All False, 2)
+
+-- It doesn't really matter what Monoid, but we need some way of combining or chosing our values.
+
+
+-- ===========================================================
+-- Tuple Monoid and Applicative Side by Side
+-- ===========================================================
+
+-- Here's how the Appicative instance is defined for a Tuple
+
+-- We're defining our own Tuple for illustration since Applicative instance for (,) already exists.
+data Tuple a b = Tuple a b deriving (Eq, Show)
+
+-- We have to implement both the Monoid and Functor instances before we can do Applicative
+
+instance (Monoid a, Monoid b) => Monoid (Tuple a b) where
+  mempty = Tuple mempty mempty
+  mappend (Tuple a b) (Tuple a' b') = Tuple (mappend a a') (mappend b b')
+
+instance Functor (Tuple a) where
+  fmap f (Tuple a b) = Tuple a (f b)
+                                                           
+instance Monoid a => Applicative (Tuple a) where
+  pure x = Tuple mempty x
+  (<*>) (Tuple a f) (Tuple a' b') = Tuple (mappend a a') (f b')
+
+t1 = Tuple "riza" 47
+t2 = Tuple "ahmed " (+1)
+
+t3 = t2 <*> t1 -- == Tuple "ahmed riza" 48
+
+-- ===========================================================
+-- Maybe Monoid and Applicative
+-- ===========================================================
+
+data MyMaybe a = MyNothing | MyJust a deriving (Eq, Show)
+
+instance Monoid a => Monoid (MyMaybe a) where
+  mempty = MyNothing
+  mappend m MyNothing = m
+  mappend MyNothing m = m
+  mappend (MyJust a) (MyJust a') = MyJust (mappend a a')
+
+instance Functor (MyMaybe) where
+  fmap _ MyNothing = MyNothing
+  fmap f (MyJust x) = MyJust (f x)
+  
+instance Applicative (MyMaybe) where
+  pure = MyJust
+  (<*>) _ MyNothing = MyNothing
+  (<*>) MyNothing _ = MyNothing
+  (<*>) (MyJust f) (MyJust a) = MyJust (f a)
+
+m1 = MyJust (Sum 20)
+m2 = MyJust (Sum 30)
+m3 = mappend m1 m2 -- == MyJust (Sum 50)
+
+-- ===========================================================
+-- Applicatives in Use
+-- ===========================================================
+
+-- List Applicative
+
+l1 = [(+1), (*2)] <*> [2, 4] -- == [3,5,4,8]
+
+xs = [1,2,3]
+ys = [4,5,6]
+
+x :: Maybe Integer
+x = lookup 3 $ zip xs ys
+
+y :: Maybe Integer
+y = lookup 2 $ zip xs ys
+
+summed :: Maybe Integer
+summed = fmap sum $ ((,) <$> x <*> y)
+
+--
+-- Identity
+--
+
+newtype Identity a = Identity a deriving (Eq, Show)
+
+type Id = Identity
+
+-- Write Applicative instance for Identity
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance Applicative Identity where
+  pure = Identity
+  (<*>) (Identity f) (Identity a) = Identity (f a)
+
+mkId = Identity
+
+ex1 :: Identity [Integer]
+ex1 = const <$> mkId xs <*> mkId ys -- == Identity [1,2,3]
+
+--
+-- Constant
+--
+
+newtype Constant a b = Constant { getConstant :: a } deriving (Eq, Ord, Show)
+
+fc = Constant (Sum 1)
+gc = Constant (Sum 2)
+
+-- Write an Applicative instance for Constant
+
+instance Functor (Constant a) where
+  fmap f (Constant a) = Constant a
+
+instance (Monoid a) => Applicative (Constant a) where
+  pure _ = Constant mempty
+  (<*>) (Constant a) (Constant a') = Constant (mappend a a')
+
+--
+-- Using the Maybe Applicative
+--
+
+validateLength :: Int -> String -> Maybe String
+validateLength maxLen s = if (length s) > maxLen then Nothing else Just s
+
+newtype Name = Name String deriving (Eq, Show)
+newtype Address = Address String deriving (Eq, Show)
+
+mkName :: String -> Maybe Name
+mkName s = fmap Name $ validateLength 25 s
+
+mkAddress :: String -> Maybe Address
+mkAddress a = fmap Address $ validateLength 100 a
+
+-- Now we'll make a smart constructor for a Person
+
+data Person = Person Name Address deriving (Eq, Show)
+
+mkPerson :: String -> String -> Maybe Person
+mkPerson n a = case mkName n of
+                 Nothing -> Nothing
+                 Just n' ->
+                   case mkAddress a of
+                     Nothing -> Nothing
+                     Just a' -> Just $ Person n' a'
+
+str = "Old MaDonald's"
+
+address :: Maybe Address
+address = mkAddress str
+
+nm :: Maybe Name
+nm = mkName "Ahmed"
+
+pf :: Maybe (Address -> Person)
+pf = fmap Person nm
+
+personMaybe :: Maybe Person
+personMaybe = pf <*> address
+
+mkPerson' :: String -> String -> Maybe Person
+mkPerson' n a = Person <$> mkName n <*> mkAddress a
+
+
+--
+
+data Cow = Cow {
+  name :: String
+  , age :: Int
+  , weight :: Int
+  } deriving (Eq, Show)
+
+cow = Cow { name = "boo", age = 2, weight = 20 }
+
+-- Example of record wild cards
+takeCow :: Cow -> Int
+takeCow Cow {..} = age * weight
+
+noEmpty :: String -> Maybe String
+noEmpty "" = Nothing
+noEmpty str = Just str
+
+nonNegative :: Int -> Maybe Int
+nonNegative n | n >= 0 = Just n
+              | otherwise = Nothing
+
+-- Validating to get rid of empty string, negative numbers
+mkCow :: String -> Int -> Int -> Maybe Cow
+mkCow name' age' weight' =
+  case noEmpty name' of
+    Nothing -> Nothing
+    Just nammy ->
+      case nonNegative age' of
+        Nothing -> Nothing
+        Just agey ->
+          case nonNegative weight' of
+            Nothing -> Nothing
+            Just weighty ->
+              Just (Cow nammy agey weighty)
+
+mkCow' :: String -> Int -> Int -> Maybe Cow
+mkCow' name age weight =
+  Cow
+  <$> (noEmpty name)
+  <*> (nonNegative age)
+  <*> (nonNegative weight)
+
+mkCow'' :: String -> Int -> Int -> Maybe Cow
+mkCow'' name age weight = liftA3 Cow (noEmpty name) (nonNegative age) (nonNegative weight)
+
+--
+-- Exercise
+--
+-- Given the function values provided, use (<$>) from Functor, (<*>) and pure from Applicative
+-- typeclass to fill in missing bits of the broken code to make it work.
+
+-- (1) const <$> Just "Hello" <*> "World"
+ex21 = const <$> Just "Hello" <*> pure "World"
+
+-- (2) (,,,) Just 90 <*> Just 10 Just "Tierness" [1,2,3]
+ex22 = (,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1,2,3]
+
+-- ===========================================================
+-- Applicative Laws
+-- ===========================================================
+
